1b641ed32af2a70a2a49ebbdc0a332f74623fea7
jymahar <jyotimahar1123@gmail.com>
2024-10-18 23:39:10 +0200

    added max limit for counter

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let counter =0;
 
+let maxLimit = 10;
+
 function decrease() {
     if(counter > 0) {
          counter-- ; 
@@ -9,8 +11,12 @@ function decrease() {
 }
 
 function increase() {
+  if (counter < maxLimit ) {
   counter++ ;  
   document.getElementById("counter").textContent = counter;
+  } else {
+    alert("You have reached maximum counter limit");
+  }
  
 }
 
